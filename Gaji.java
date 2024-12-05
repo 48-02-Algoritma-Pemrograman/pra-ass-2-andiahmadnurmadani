407c5af70b82417658b181ef33c6ae7c4dbac679
andiahmadnurmadani <106458721+andiahmadnurmadani@users.noreply.github.com>
2024-12-05 13:24:24 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,7 +1,32 @@
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class Gaji{
     public static int masaKerja;
+
+    static int gaji() {
+      int gaji;
+      if (masaKerja < 5) {
+        gaji = 5000000;
+      } else if (masaKerja >= 5 && masaKerja <= 10){
+        gaji = 7500000;
+      } else {
+        gaji = 10000000;
+      }
+      return gaji;
+    }
+
+    static int lembur(int jumlahJam) {
+      int honor;
+      if (masaKerja < 5) {
+        honor = 40000 * jumlahJam;
+      } else if (masaKerja >= 5 && masaKerja <= 10) {
+        honor = 50000 * jumlahJam;
+      } else {
+        honor = 75000 * jumlahJam;
+      }
+      return honor;
+
+    }
     public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
@@ -9,7 +34,8 @@ public class Gaji{
         int jamLembur = input.nextInt();
 
         /* Pekerjaan anda mulai dari sini */
-
+        int hasil = gaji() + lembur(jamLembur);
+        System.out.println(hasil);
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Gaji.class.getDeclaredMethods().length);
